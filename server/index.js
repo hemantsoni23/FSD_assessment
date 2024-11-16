5fcd0c9ac06715af51ce74ecf6b7f0558e5c66cd
Hemant soni <sonih5190@gmail.com>
2024-11-17 00:37:58 +0530

    remove cors from index.js
    Removing cors from index.js because backend and frontend are deployed on different domains on vercel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
@@ -11,16 +11,17 @@ dotenv.config()
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-// CORS options
-const corsOptions = {
-  origin: ['https://fsd-assessment-lzds6d243-hemantsoni42s-projects.vercel.app', process.env.FRONTEND_URL], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true,
-};
+//removing cors fue to frontend and backend are on different domains on vercel
+// // CORS options 
+// const corsOptions = {
+//   origin: ['https://fsd-assessment-lzds6d243-hemantsoni42s-projects.vercel.app', process.env.FRONTEND_URL], 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+//   allowedHeaders: ['Content-Type', 'Authorization'], 
+//   credentials: true,
+// };
 
-// Use CORS middleware
-app.use(cors(corsOptions));
+// // Use CORS middleware
+// app.use(cors(corsOptions));
 
 // Placeholder route
 app.get('/', (req, res) => {
