ba085adb88ab67aea0fd4e30219b4a704ddb58d7
Hemant soni <sonih5190@gmail.com>
2024-11-17 00:47:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
